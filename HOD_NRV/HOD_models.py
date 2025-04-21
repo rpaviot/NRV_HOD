c7007b0b0c7aa450b7abad7749334ff139388197
rpaviot <romainpaviot@gmail.com>
2025-04-21 20:40:29 +0200

    gitignore

--- HOD_NRV/HOD_models.py
+++ HOD_NRV/HOD_models.py
@@ -36,14 +36,14 @@ def HOD_satellite(logM, As, Mmin, M1, alpha, kappa):
     return Nsat
 
 
-def compute_ngal(logM,mass_function,probC,probS):
+def compute_ngal_(logM,mass_function,probC,probS):
     integrand = mass_function*(probC + probS)
     func_intg = CS(logM,integrand)
     ngal = gauss_legendre_integration(func_intg,logM.min(),logM.max())
     return ngal
     
-def compute_fsat(logM,mass_function,probC,probS):
-    ngal = compute_ngal(logM,mass_function,probC,probS)
+def compute_fsat_(logM,mass_function,probC,probS):
+    ngal = compute_ngal_(logM,mass_function,probC,probS)
     integrand = mass_function*probS
     func_intg = CS(logM,integrand)
     nsat = gauss_legendre_integration(func_intg,logM.min(),logM.max())
@@ -59,7 +59,7 @@ class Occupation:
 
     satellite_params = ["As", "Mmin", "M1", "alpha", "kappa"]
 
-    def __init__(self, hod_type):
+    def __init__(self, hod_type,cosmo_params,z_snap):
         if hod_type not in self.central_funcs:
             raise ValueError(f"Unknown HOD type: {hod_type}")
         self.hod_type = hod_type
@@ -67,16 +67,23 @@ class Occupation:
         self.HOD_satellite = HOD_satellite
         
         params = {}
-        self.params = {key: params.get(key, None) for key in self.central_params + self.satellite_params}
-    
+        #self.params = {key: params.get(key, None) for key in self.central_params + self.satellite_params}
+        self.key = {key for key in self.central_params + self.satellite_params}
+        self.logM_bins = jnp.geomspace(10.6,15,10000)
+
+        self.mass_function = set_massfunction(cosmo_params,self.logM_bins,z=z_snap)
+
+
     def set_params(self, dict_params):
         try:
-            [dict_params[key] for key in self.params]
+            [dict_params[key] for key in self.key]
         except KeyError as e:
             raise ValueError(f"Missing an argument: {e.args[0]}")
         self.central_args, self.satellite_args = (
             [dict_params[key] for key in self.central_params],
             [dict_params[key] for key in self.satellite_params])
+        
+        self.params = dict_params
         
         
     def compute_HOD_occupation(self,logM,dict_params):
@@ -85,3 +92,14 @@ class Occupation:
         probS = self.HOD_satellite(logM, *self.satellite_args)
     
         return probC,probS
+    
+    def compute_ngal(self,dict_params):
+        probC,probS = self.compute_HOD_occupation(self.logM_bins,dict_params)
+        ngal = compute_ngal_(self.logM_bins,self.mass_function,probC,probS)
+        return ngal
+
+    def compute_fsat(self,dict_params):
+        probC,probS = self.compute_HOD_occupation(self.logM_bins,dict_params)
+        fsat = compute_fsat_(self.logM_bins,self.mass_function,probC,probS)
+        return fsat
+    
--- HOD_NRV/populate_halo.py
+++ HOD_NRV/populate_halo.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np 
+#import jax.numpy as jnp
+from .utils import * 
+from .NFW import * 
+
 
 class halo_occupation:
     
     """ 
     This is the main class that populate dark halos of a dark matter simulation with galaxies
     """
+
+    
+
+
